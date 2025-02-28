89120cef7eee9f81b16f47226c2375681ac6d663
Shubhamkumar000 <annonminecraft@gmail.com>
2025-02-28 17:01:51 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,10 @@ import orderRouter from './route/order.route.js'
 
 
 const app = express() 
-// app.use(cors({
-//     credentials : true,
-//     origin : process.env.FRONTEND_URL
-// }))
-app.use(cors())
+app.use(cors({
+    credentials : true,
+    origin : process.env.FRONTEND_URL
+}))
 
 app.use(express.json())
 app.use(cookieParser())
